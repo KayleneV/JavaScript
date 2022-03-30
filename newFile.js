23eb923a73582c7a0ba515295759a612febd06a4
Kaylene Van Wyk <95338328+KayleneV@users.noreply.github.com>
2022-03-30 16:33:15 +0200

    started javascript

---
+++ newFile.js
@@ -0,0 +1,46 @@
+// TITLE
+console.log(" ----- Area Of Shapes JS Arithmetic Challenge ----- \n\n")
+
+// Constant Variables:
+const pi = 3.1419;
+const half = 0.5;
+
+// Variables for measurements
+
+const rectangleLength = 10;
+const rectangleBreadth = 17;
+const circleRadius = 20;
+const triangleBase = 20;
+const triangleHeight = 9;
+
+console.log("rectangleLength is:" + rectangleLength);
+console.log("rectangleBreadth is:" + rectangleBreadth);
+console.log("circleRadius is:" + circleRadius);
+
+
+
+// Rectangle area calculation
+   //replace with calculation code here..
+
+// Append your rectangle answer:
+console.log("The area of the given rectangle is : " );
+
+
+console.log("\n");
+
+
+// circle area calculation
+   // replace with calculation code here..
+
+// Append your circle answer:
+console.log("The area of the given circle is : ");
+
+
+console.log("\n");
+
+
+// triangle area calculation
+   // replace with calculation code here..
+
+// Append your triangle answer:
+console.log("The area of the given triangle is : " );
